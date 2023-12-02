0f9fb222ac3d14c834a26c2c8c27ab54f2fb725b
fury0us <139115148+Fury0us@users.noreply.github.com>
2023-12-02 03:39:31 +0000

    1st Update
    Added more functionality as well as the ability to ping google, and install oh-my-zsh directly from the script.

--- muxman.sh
+++ muxman.sh
@@ -3,13 +3,20 @@
 main_menu() {
 COLUMNS=12
 PS3=("#: ")
-options=("list IP address" "list MAC address" "update and upgrade" "termux config" "Exit")
+options=("Ping Google" "list IP address" "list MAC address" "update and upgrade" "termux config" "Exit")
 select opt in "${options[@]}"; do
 case $opt in
+          "Ping Google")
+          ping google.com
+          asci_intro
+          main_menu
+          COLUMNS=12
+          ;;
           "list IP address")
           echo -en '\n'
           ifconfig wlan0 | grep inet
           echo -en '\n'
+          asci_intro
           main_menu
           COLUMNS=12
           ;;
@@ -17,6 +24,7 @@ case $opt in
           echo -en '\n'
           ifconfig wlan0 | grep spec
           echo -en '\n'
+          asci_intro
           main_menu
           COLUMNS=12
           ;;
@@ -24,6 +32,7 @@ case $opt in
           echo -en '\n'
           pkg update && pkg upgrade -y
           echo -en '\n'
+          asci_intro
           main_menu
           COLUMNS=12
           ;;
@@ -33,6 +42,7 @@ case $opt in
           echo -en '\n'
           credits
           echo -en '\n'
+          asci_intro
           termux_config
           COLUMNS=12
           ;;
@@ -47,7 +57,7 @@ done
 termux_config() {
 COLUMNS=12
 PS3=("#: ")
-options=("Setup Termux" "Remove Default Termux Start Message" "Search For Package" "Install Package" "Change Repo" "Install X11 Repo" "Install Root Repo" "Main Menu" "Exit")
+options=("Setup Termux" "Remove Default Termux Start Message" "Install oh-my-zsh" "View Installed Packages" "Search For Package" "Install rxfetch(neofetch)" "Install Package" "Change Repo" "Install X11 Repo" "Install Root Repo" "Main Menu" "Exit")
 select opt in "${options[@]}"; do
 case $opt in
           "Setup Termux")
@@ -61,6 +71,7 @@ case $opt in
           echo $'\e[31mTermux Has Been Set Up Successfully!\e[0m'
           sleep 2
           clear
+          asci_intro
           termux_config
           COLUMNS=12
           ;;
@@ -84,33 +95,66 @@ case $opt in
                echo $'\e[1;31mEDIT VIA usr/etc/bash.bashrc file \e[0m'
                sleep 3
           fi
+          asci_intro
+          termux_config
+          COLUMNS=12
+          ;;
+          "Install oh-my-zsh")
+          read -p $'\e[31mAre you sure?(yes/no)\e[0m' choicee
+          if [ $choicee = "yes" ] || [ $choicee = "y" ]
+          then
+               sh -c "$(curl -fsSL https://github.com/Cabbagec/termux-ohmyzsh/raw/master/install.sh)"
+          elif [ $choicee = "no" ] || [ $choicee = "n" ]
+          then
+               termux_config
+          fi
+          asci_intro
+          termux_config
+          COLUMNS=12
+          ;;
+          "View Installed Packages")
+          pkg list-installed
+          asci_intro
           termux_config
           COLUMNS=12
           ;;
           "Search For Package")
           read -p $'\e[31mQuery?: \e[0m' package
           pkg search $package
+          asci_intro
+          termux_config
+          COLUMNS=12
+          ;;
+          "Install rxfetch(neofetch)")
+          pkg install rxfetch
+          clear
+          rxfetch
+          asci_intro
           termux_config
           COLUMNS=12
           ;;
           "Install Package")
           read -p $'\e[31mPackage?: \e[0m' package
           pkg install $package
+          asci_intro
           termux_config
           COLUMNS=12
           ;;
           "Change Repo")
           termux-change-repo
+          asci_intro
           termux_config
           COLUMNS=12
           ;;
           "Install X11 Repo")
           pkg install x11-repo
+          asci_intro
           termux_config
           COLUMNS=12
           ;;
           "Install Root Repo")
           pkg install root-repo
+          asci_intro
           termux_config
           COLUMNS=12
           ;;
@@ -133,13 +177,13 @@ done
 #start asci intro here
 clear
 asci_intro() {
-echo $'\e[32m *********************************************'
-echo $'\e[32m * __  __                 __  __             *'
-echo $'\e[32m *|  \/  |_   ___  __    |  \/  | __ _ _ __  *'
-echo $'\e[32m *| |\/| | | | \ \/ /____| |\/| |/ _` | `_ \ *'
-echo $'\e[32m *| |  | | |_| |>  <_____| |  | | (_| | | | |*'
-echo $'\e[32m *|_|  |_|\__,_/_/\_\    |_|  |_|\__,_|_| |_|*'
-echo $'\e[32m *********************************************'
+echo $'\e[32m *********************************************\e[0m'
+echo $'\e[32m * __  __                 __  __             *\e[0m'
+echo $'\e[32m *|  \/  |_   ___  __    |  \/  | __ _ _ __  *\e[0m'
+echo $'\e[32m *| |\/| | | | \ \/ /____| |\/| |/ _` | `_ \ *\e[0m'
+echo $'\e[32m *| |  | | |_| |>  <_____| |  | | (_| | | | |*\e[0m'
+echo $'\e[32m *|_|  |_|\__,_/_/\_\    |_|  |_|\__,_|_| |_|*\e[0m'
+echo $'\e[32m *********************************************\e[0m'
 }
 credits() {
 echo $'\e[36m╭━━╮╱╱╱╭━╮╱╱╱╱╱╭━━━╮╱╭━╮\e[0m'
