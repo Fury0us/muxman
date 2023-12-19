2e2f21a23fcc97ddfeb236e7f4c207a361c6eae1
fury0us <139115148+Fury0us@users.noreply.github.com>
2023-12-18 17:20:22 -0700

    2nd Update

--- muxman.sh
+++ muxman.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 #start the main menu function
 main_menu() {
+asci_intro
+echo -en '\n'
+credits
+echo -en '\n'
 COLUMNS=12
 PS3=("#: ")
-options=("Ping Google" "list IP address" "list MAC address" "update and upgrade" "termux config" "Exit")
+options=("Ping Google" "list IP address" "list MAC address" "update and upgrade" "termux config" "SU MENU(ROOT)" "Exit")
 select opt in "${options[@]}"; do
 case $opt in
           "Ping Google")
           ping google.com
-          asci_intro
           main_menu
           COLUMNS=12
           ;;
@@ -16,7 +19,6 @@ case $opt in
           echo -en '\n'
           ifconfig wlan0 | grep inet
           echo -en '\n'
-          asci_intro
           main_menu
           COLUMNS=12
           ;;
@@ -24,7 +26,6 @@ case $opt in
           echo -en '\n'
           ifconfig wlan0 | grep spec
           echo -en '\n'
-          asci_intro
           main_menu
           COLUMNS=12
           ;;
@@ -32,18 +33,19 @@ case $opt in
           echo -en '\n'
           pkg update && pkg upgrade -y
           echo -en '\n'
-          asci_intro
           main_menu
           COLUMNS=12
           ;;
           "termux config")
           clear
-          asci_intro
           echo -en '\n'
-          credits
-          echo -en '\n'
-          asci_intro
           termux_config
+          COLUMNS=12
+          ;;
+          "SU MENU(ROOT)")
+          clear
+          echo -en '\n'
+          surootmenu
           COLUMNS=12
           ;;
           "Exit")
@@ -54,6 +56,91 @@ esac
 done
 }
 #termux_config section
+surootmenu() {
+echo -en '\n'
+asci_intro
+echo -en '\n'
+root_art
+echo -en '\n'
+PS3=("#super: ")
+surtmnu=("SU session" "Device Options" "Auditing Tools" "Main Menu" "Exit")
+select surt in "${surtmnu[@]}"; do
+case $surt in
+"SU session")
+echo -en '\n'
+echo $'\e[1;31mtype "exit" to return to terminal! \e[0m'
+echo -en '\n'
+su
+;;
+"Device Options")
+clear
+asci_intro
+dev_opt
+COLUMNS=12
+;;
+"Auditing Tools")
+clear
+asci_intro
+aud_tools
+COLUMNS=12
+;;
+"Main Menu")
+clear
+main_menu
+COLUMNS=12
+;;
+"Exit")
+clear
+exit
+;;
+esac
+done
+}
+aud_tools() {
+PS3=("#aud: ")
+}
+dev_opt() {
+COLUMNS=12
+PS3=("#devopt: ")
+devopt=("Change All Device Information" "IP Menu" "MAC Menu" "Main" "Exit")
+select devo in "${devopt[@]}"; do
+case $devo in
+"Change All Device Information")
+clear
+change_dev_info
+;;
+"IP Menu")
+clear
+asci_intro
+ip_menu
+COLUMNS=12
+;;
+"MAC Menu")
+clear
+asci_intro
+mac_menu
+COLUMNS=12
+;;
+"Main")
+clear
+main_menu
+;;
+"Exit")
+clear
+exit
+;;
+esac
+done
+}
+mac_menu() {
+echo "mac menu"
+}
+ip_menu() {
+echo "ip menu"
+}
+change_dev_info() {
+echo "spoof device"
+}
 termux_config() {
 COLUMNS=12
 PS3=("#: ")
@@ -192,10 +279,12 @@ echo $'\e[36m┃╭╮┃┃┣┫┃╭┫┃┃╭┫┃┃┃┃┃┃┃┣�
 echo $'\e[36m╰━━╋╮┣╯╰╯╰━┻╯┣╮┃┃┃┃┣━┻━╯\e[0m'
 echo $'\e[36m╱╱╱╰━╯╱╱╱╱╱╱╱╰━┫╰━╯┃\e[0m'
 }
-asci_intro
-sleep 0.5
-echo -en '\n'
-credits
-sleep 0.5
-echo -en '\n'
+root_art() {
+echo $'\e[1;31m______ _____  _____ _____ \e[0m'
+echo $'\e[1;31m| ___ \  _  ||  _  |_   _|\e[0m'
+echo $'\e[1;31m| |_/ / | | || | | | | |  \e[0m'
+echo $'\e[1;31m|    /| | | || | | | | |  \e[0m'
+echo $'\e[1;31m| |\ \| \_/ /\ \_/ / | |  \e[0m'
+echo $'\e[1;31m\_| \_|\___/  \___/  \_/  \e[0m'
+}
 main_menu
